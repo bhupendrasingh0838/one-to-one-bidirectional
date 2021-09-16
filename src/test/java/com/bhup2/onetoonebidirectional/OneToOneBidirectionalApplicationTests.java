d7805eb1d8dd88c74a4de7958b43d398d5f969e5
bhupendrasingh0838 <bhupendrasingh0838@gmail.com>
2021-09-16 17:11:15 +0530

    version

---
+++ src/test/java/com/bhup2/onetoonebidirectional/OneToOneBidirectionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhup2.onetoonebidirectional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneBidirectionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
